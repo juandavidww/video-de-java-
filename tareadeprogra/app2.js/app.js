ab6e225a8a8d5129413599aaea12d92047166860
juandavidww <perro123alabrosterxd@gmail.com>
2024-09-03 23:02:54 -0500

    Add files via upload

---
+++ tareadeprogra/app2.js/app.js
@@ -0,0 +1,29 @@
+// tipos de datos
+"Hello World" // string
+'Hello World' // string
+
+1000000 // number
+-2.3
+
+// boolean
+true
+false
+
+// array
+['joe', 'ryan', 'martha']
+[1, 2, 3]
+[true, false, true, false]
+
+// object
+'ryan'
+70.4
+14
+true
+
+{
+    "usarname" : 'ryan',
+    "score" : 70.4,
+    "hours" : 14,
+    "proffesional" : true,
+    "friends" : true
+}
